4a7c4d372ca6362a92363f18aab1c9575c10e4df
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2022-03-25 10:56:30 +0100

    Update RW2022 script

--- source/rust_verify/example/rw2022/script.rs
+++ source/rust_verify/example/rw2022/script.rs
@@ -94,7 +94,6 @@ fn test_prime(candidate: u64) -> bool {
     true
 }
 
-#[proof]
 fn assertions() {
     assert(divides(3, 6));
     assert(divides(12, 24));
@@ -104,6 +103,83 @@ fn assertions() {
     assert(is_prime(5));
 }
 
-// ## 15 -- 15-fibo.rs
+// ## 15-16 -- 15-16-fibo.rs
 
+#[spec]
+fn fibo(n: nat) -> nat {
+  decreases(n);
+  if n == 0 { 0 } else if n == 1 { 1 }
+  else { fibo(n - 2) + fibo(n - 1) }
+}
 
+#[proof]
+fn lemma_fibo_is_monotonic(i:nat, j:nat) {
+  requires(i<=j);
+  ensures(fibo(i) <= fibo(j));
+  decreases(j-i);
+
+  // ----
+
+  if i<2 && j<2 { } else if i==j { }
+  else if i==j-1 {
+    reveal_with_fuel(fibo, 2);
+    lemma_fibo_is_monotonic(i, j-1);
+  } else {
+    lemma_fibo_is_monotonic(i, j-1);
+    lemma_fibo_is_monotonic(i, j-2);
+  }
+}
+
+#[spec]
+fn fibo_fits_u64(n: nat) -> bool { fibo(n) <= 0xffff_ffff_ffff_ffff }
+
+#[exec]
+fn fibo_impl(n: u64) -> u64 {
+  requires(fibo_fits_u64(n));
+  ensures(|result: u64| result == fibo(n));
+
+  // ----
+
+  if n == 0 { return 0; }
+  let mut prev: u64 = 0; let mut cur: u64 = 1;
+  let mut i: u64 = 1;
+  while i < n {
+    invariant([
+      i > 0, i <= n,
+      fibo_fits_u64(n as nat), fibo_fits_u64(i as nat),
+      cur == fibo(i), prev == fibo(i as nat - 1),
+    ]);
+    let new_cur = cur + prev;
+    prev = cur; cur = new_cur; i = i + 1;
+    lemma_fibo_is_monotonic(i, n);
+  }
+  cur
+}
+
+// ## 18 -- 18-reverse.rs -- spec variables
+
+fn reverse(v: &mut Vec<u64>) {
+    ensures([
+        v.len() == old(v).len(),
+        forall(|i: int| 0 <= i && i < old(v).len() >>= v.index(i) == old(v).index(old(v).len() - i - 1)),
+    ]);
+
+    let length = v.len();
+    #[spec] let v1 = *v;
+    let mut n: usize = 0;
+    while n < length / 2 {
+        invariant([
+            length == v.len(),
+            forall(|i: int| n <= i && i + n < length >>= v.index(i) == v1.index(i)),
+            forall(|i: int| 0 <= i && i < n >>= v.index(i) == v1.index(length - i - 1)),
+            forall(|i: int| 0 <= i && i < n >>= v1.index(i) == v.index(length - i - 1)),
+        ]);
+
+        let x = *v.index(n);
+        let y = *v.index(length - 1 - n);
+        v.set(n, y);
+        v.set(length - 1 - n, x);
+
+        n = n + 1;
+    }
+}
